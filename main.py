5e343c2a1f41073400906460510537e443c4fc0d
hense96 <36164168+hense96@users.noreply.github.com>
2018-02-05 15:34:37 +0000

    Add files via upload
    file and class structure

---
+++ main.py
@@ -0,0 +1,32 @@
+import tree
+
+# the learning function, see table 1 on page 13
+def decision_tree_learning( data, attributes, targets ):
+    # TODO
+    # param data: matrix of data according to specification on page 13
+    # param attributes: vector of attributes according to specification on page 13
+    # param binary_targets: target vector according to specification on page 13
+    # return: the learned decision tree, i.e. an object of class Tree
+    pass
+
+
+# sub-function used in learning function
+def choose_best_decision_attribute( data, attributes, binary_targets ):
+    # TODO
+    # param data: matrix of data according to specification on page 13
+    # param attributes: vector of attributes according to specification on page 13
+    # param binary_targets: target vector according to specification on page 13
+    # return: the attribute number
+    pass
+
+
+# sub-function used in learning function
+def majority_value( binary_targets ):
+    # TODO
+    # param binary_targets: target vector according to specification on page 13
+    # return: the mode of the binary targets
+    pass
+
+
+
+# TODO: implement evaluation part
---
+++ tree.py
@@ -0,0 +1,39 @@
+class Tree:
+
+    # the initialisation function
+    def __init__(self):
+
+        # TODO
+
+        # the emotion the tree represents
+        label = ...
+        # the root node
+        root = ...
+
+        pass
+
+    # a function to create a visualisation of the tree
+    def visualise(self):
+        # TODO
+        pass
+
+
+class Node:
+
+    # the initialisation function
+    def __init__(self):
+
+        # TODO
+
+        # a reference to the parent node
+        parent = ...
+        # an array of references to the child nodes
+        # child[0] refers to the child node for action unit turned off
+        # child[1] refers to the child node for action unit turned on
+        child = ...
+        # the number of the action unit the node contains
+        attribute = ...
+        # see tree.class on page 14 of the manual
+        classvalue = ...
+
+        pass
